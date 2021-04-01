ef1ba2c628477fe050f89a6be6c744520b5366f0
thegabriele97 <gabriele.lagreca@gmail.com>
2021-04-01 12:17:01 +0200

    alap algorithm code clean

--- lab2/lab2.1.tcl
+++ lab2/lab2.1.tcl
@@ -9,20 +9,20 @@ proc alap {lambda} {
 
     foreach node [ lreverse [get_sorted_nodes] ] {
         set ldummy {}
-        set op_delay [ get_attribute [ get_lib_fu_from_op [ get_attribute $node operation ] ] delay ]
 
+        set op_delay [ get_attribute [ get_lib_fu_from_op [ get_attribute $node operation ] ] delay ]
         set minval $lambda
         # all nodes with 0 children will be scheduled at the end [$lambda - $op_delay]
         # so this for will be skipped because they have 0 children
         foreach child [ get_attribute $node children ] {
             set searchr [ lindex $result [ lsearch -index 0 $result $child ]]
-            if {[ lindex $searchr 1 ] < $minval } {
-                set minval [ lindex $searchr 1 ]
+            set starttime [ lindex $searchr 1 ] 
+            if {$starttime < $minval } {
+                set minval $starttime
             }
         }
 
         set result [ lappend result [ lappend ldummy $node [ expr $minval - $op_delay ] ] ]
-
     }
 
     return [ lreverse $result ]
